f48ec5f28e0a8ac5f668abc320aea089f29b5b07
rodrigocitadin <citadinrodrigo@gmail.com>
2023-01-09 01:14:02 -0300

    finished another rust book challenge :)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("\nDigite um inteiro maior que zero\n");
+
+        let mut n: String = String::new();
+
+        io::stdin().read_line(&mut n).expect("Falha ao ler entrada");
+
+        let n: u64 = match n.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Valor invalido!");
+                continue;
+            }
+        };
+
+        let result: u64 = fibonacci(n);
+
+        println!("\nO {}° termo da sequência de fibonacci é {}", n, result);
+    }
+}
+
+fn fibonacci(n: u64) -> u64 {
+    if n == 1 {
+        0
+    } else {
+        if n == 2 {
+            1
+        } else {
+            fibonacci(n - 1) + fibonacci(n - 2)
+        }
+    }
 }
